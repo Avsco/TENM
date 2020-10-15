9e6c916fa98aa0652cc828b116c74deb70132e39
Avsco <benacho82@gmail.com>
2020-10-15 11:43:31 -0400

    refactor: add handlerWrror & remove arrow function

--- src/modules/users/dto.ts
+++ src/modules/users/dto.ts
@@ -1,12 +1,10 @@
-/* Los parametros que recivo del controlador despues de que vea obtenido la  
-respuesta de la base de datos aqui los transformo para poder mandarlos bonito*/
 import { Interface } from "./modelSchema";
 
-class Dto {
+class UserDto {
     public single = (model: Interface) => ({
       name: model.name,
       usermane: model.username
     })
 }
 
-export default new Dto()
+export default new UserDto()
--- src/modules/users/modelSchema.ts
+++ src/modules/users/modelSchema.ts
@@ -1,16 +1,12 @@
-  
 import { model, Schema, Document } from 'mongoose'
-import bcrypt from 'bcrypt'
-
 
 export interface Interface extends Document {
     name: string
     username: string
     password: string
-    comparePassword: (password: string) => Promise<boolean>
-    encryptPassword: (password: string) => Promise<string>
 }
 
+//add validators
 const modelSchema: Schema = new Schema(
     {
         name: {
@@ -26,27 +22,8 @@ const modelSchema: Schema = new Schema(
             type: String,
             required: true,
         },
-
     },
     { versionKey: false }
 )
-
-modelSchema.pre<Interface>('save', async function (next) {
-    const schema = this
-    if (!schema.isModified('password')) return next()
-
-    schema.password = await schema.encryptPassword(schema.password)
-    next()
-})
-
-modelSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
-    return await bcrypt.compare(password, this.password)
-}
-
-modelSchema.methods.encryptPassword = async function (password: string): Promise<string> {
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-    return hash
-}
 
 export default model<Interface>('users', modelSchema)
