0b17410ebef07ed334ffdcabae5ad94f196daf09
Alan Benavides <benacho82@gmail.com>
2021-04-01 14:37:47 -0400

    remove: dto

--- src/modules/users/controller.ts
+++ src/modules/users/controller.ts
@@ -1,22 +1,21 @@
 import { Request, Response } from "express";
-import Dto from "./dto";
 import Model from "./model";
 
 class Controller {
-  public show = async (req: Request, res: Response): Promise<Response> => {
+  show = async (req: Request, res: Response): Promise<Response> => {
     try {
       const id = req.params.id;
 
       const model = await Model.show(id);
       if (!model) return res.status(404).json({ msg: "El recurso no existe" });
 
-      return res.status(200).json(Dto.single(model));
+      return res.status(200).json(model);
     } catch (error) {
       return res.status(422).json({ code: error.code, msg: error.message });
     }
   };
 
-  public put = async (req: Request, res: Response): Promise<Response> => {
+  put = async (req: Request, res: Response): Promise<Response> => {
     try {
       let model: any = {};
       if (req.body.name) model.name = req.body.name;
@@ -27,13 +26,13 @@ class Controller {
       if (!newModel)
         return res.status(404).json({ msg: "El recurso no existe" });
 
-      return res.status(200).json(Dto.single(newModel));
+      return res.status(200).json(newModel);
     } catch (error) {
       return res.status(422).json({ code: error.code, msg: error.message });
     }
   };
 
-  public signUp = async (req: Request, res: Response): Promise<Response> => {
+  signUp = async (req: Request, res: Response): Promise<Response> => {
     try {
       if (!req.body.name)
         return res.status(400).json({ msg: "falta el nombre" });
@@ -50,13 +49,13 @@ class Controller {
       if (!newModel)
         return res.status(500).json({ msg: "El recuso no pudo ser creado" });
 
-      return res.status(201).json(Dto.single(newModel));
+      return res.status(201).json(newModel);
     } catch (error) {
       return res.status(422).json({ code: error.code, msg: error.message });
     }
   };
 
-  public signIn = async (req: Request, res: Response) => {
+  signIn = async (req: Request, res: Response) => {
     try {
       if (!req.body.username)
         return res.status(400).json({ msg: "falta el username" });
@@ -67,7 +66,7 @@ class Controller {
       if (!model)
         return res.status(404).json({ msg: "Erorr en username o password" });
 
-      return res.status(200).json(Dto.single(model));
+      return res.status(200).json(model);
     } catch (error) {
       return res.status(422).json({ code: error.code, msg: error.message });
     }
